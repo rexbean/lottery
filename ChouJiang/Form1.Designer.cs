27650c0033dedc8cc6064ccf7f43238b9460ed53
rexbean <rexbean36@gmail.com>
2017-03-21 09:21:07 -0700

    lottery program

---
+++ ChouJiang/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ChouJiang
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.DepComBox = new System.Windows.Forms.ComboBox();
+            this.DepLabel = new System.Windows.Forms.Label();
+            this.RewardStaffLabel = new System.Windows.Forms.Label();
+            this.AllLabel = new System.Windows.Forms.Label();
+            this.CurrentStaffLabel = new System.Windows.Forms.Label();
+            this.FirstLabel = new System.Windows.Forms.Label();
+            this.SecondLabel = new System.Windows.Forms.Label();
+            this.ThirdLabel = new System.Windows.Forms.Label();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(35, 212);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "开始";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(159, 212);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "停止";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // DepComBox
+            // 
+            this.DepComBox.FormattingEnabled = true;
+            this.DepComBox.Location = new System.Drawing.Point(95, 17);
+            this.DepComBox.Name = "DepComBox";
+            this.DepComBox.Size = new System.Drawing.Size(200, 20);
+            this.DepComBox.TabIndex = 3;
+            this.DepComBox.SelectedIndexChanged += new System.EventHandler(this.DepComBox_SelectedIndexChanged);
+            this.DepComBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.DepComBox_KeyPress);
+            // 
+            // DepLabel
+            // 
+            this.DepLabel.AutoSize = true;
+            this.DepLabel.BackColor = System.Drawing.Color.Transparent;
+            this.DepLabel.Location = new System.Drawing.Point(12, 17);
+            this.DepLabel.Name = "DepLabel";
+            this.DepLabel.Size = new System.Drawing.Size(65, 12);
+            this.DepLabel.TabIndex = 4;
+            this.DepLabel.Text = "请选择部门";
+            // 
+            // RewardStaffLabel
+            // 
+            this.RewardStaffLabel.AutoSize = true;
+            this.RewardStaffLabel.BackColor = System.Drawing.Color.Transparent;
+            this.RewardStaffLabel.Location = new System.Drawing.Point(12, 89);
+            this.RewardStaffLabel.Name = "RewardStaffLabel";
+            this.RewardStaffLabel.Size = new System.Drawing.Size(77, 12);
+            this.RewardStaffLabel.TabIndex = 8;
+            this.RewardStaffLabel.Text = "被抽到的员工";
+            // 
+            // AllLabel
+            // 
+            this.AllLabel.AutoSize = true;
+            this.AllLabel.BackColor = System.Drawing.Color.Transparent;
+            this.AllLabel.Font = new System.Drawing.Font("宋体", 60F, System.Drawing.FontStyle.Bold);
+            this.AllLabel.Location = new System.Drawing.Point(95, 53);
+            this.AllLabel.Name = "AllLabel";
+            this.AllLabel.Size = new System.Drawing.Size(0, 80);
+            this.AllLabel.TabIndex = 9;
+            // 
+            // CurrentStaffLabel
+            // 
+            this.CurrentStaffLabel.AutoSize = true;
+            this.CurrentStaffLabel.BackColor = System.Drawing.Color.Transparent;
+            this.CurrentStaffLabel.Location = new System.Drawing.Point(12, 53);
+            this.CurrentStaffLabel.Name = "CurrentStaffLabel";
+            this.CurrentStaffLabel.Size = new System.Drawing.Size(53, 12);
+            this.CurrentStaffLabel.TabIndex = 10;
+            this.CurrentStaffLabel.Text = "当前员工";
+            // 
+            // FirstLabel
+            // 
+            this.FirstLabel.AutoSize = true;
+            this.FirstLabel.BackColor = System.Drawing.Color.Transparent;
+            this.FirstLabel.Location = new System.Drawing.Point(96, 88);
+            this.FirstLabel.Name = "FirstLabel";
+            this.FirstLabel.Size = new System.Drawing.Size(0, 12);
+            this.FirstLabel.TabIndex = 11;
+            // 
+            // SecondLabel
+            // 
+            this.SecondLabel.AutoSize = true;
+            this.SecondLabel.BackColor = System.Drawing.Color.Transparent;
+            this.SecondLabel.Location = new System.Drawing.Point(102, 120);
+            this.SecondLabel.Name = "SecondLabel";
+            this.SecondLabel.Size = new System.Drawing.Size(0, 12);
+            this.SecondLabel.TabIndex = 12;
+            // 
+            // ThirdLabel
+            // 
+            this.ThirdLabel.AutoSize = true;
+            this.ThirdLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ThirdLabel.Location = new System.Drawing.Point(98, 163);
+            this.ThirdLabel.Name = "ThirdLabel";
+            this.ThirdLabel.Size = new System.Drawing.Size(0, 12);
+            this.ThirdLabel.TabIndex = 13;
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.TitleLabel.Location = new System.Drawing.Point(192, 16);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(143, 12);
+            this.TitleLabel.TabIndex = 14;
+            this.TitleLabel.Text = "IEMS SEDA嘉年华竞赛活动";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.ThirdLabel);
+            this.Controls.Add(this.SecondLabel);
+            this.Controls.Add(this.FirstLabel);
+            this.Controls.Add(this.CurrentStaffLabel);
+            this.Controls.Add(this.AllLabel);
+            this.Controls.Add(this.RewardStaffLabel);
+            this.Controls.Add(this.DepLabel);
+            this.Controls.Add(this.DepComBox);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "MainForm";
+            this.Text = "抽奖";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.ComboBox DepComBox;
+        private System.Windows.Forms.Label DepLabel;
+        private System.Windows.Forms.Label RewardStaffLabel;
+        private System.Windows.Forms.Label AllLabel;
+        private System.Windows.Forms.Label CurrentStaffLabel;
+        private System.Windows.Forms.Label FirstLabel;
+        private System.Windows.Forms.Label SecondLabel;
+        private System.Windows.Forms.Label ThirdLabel;
+        private System.Windows.Forms.Label TitleLabel;
+    }
+}
+
---
+++ ChouJiang/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ChouJiang
+{
+    public partial class MainForm : Form
+    {
+        int i = 0;
+        int j = 0;
+
+        string DepAName = null;
+        string DepBName = null;
+        string DepCName = null;
+        string DepDName = null;
+
+        List<String> DepAStaff = new List<string>();
+        List<String> DepBStaff = new List<string>();
+        List<String> DepCStaff = new List<string>();
+        List<String> DepDStaff = new List<string>();
+
+        List<String> CurrentList;
+
+        List<Label> RewardLabelList = new List<Label>();
+
+        Label CurrentLabel;
+
+        List<List<String>> allList = new List<List<string>>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            SetForm();
+            SetLabel();
+            ReadFileIn();
+            SetComboBox();
+            SetList();
+        }
+
+        private void SetForm()
+        {
+            string path = Application.StartupPath+"\\bg.jpg";
+            Rectangle rect = System.Windows.Forms.SystemInformation.VirtualScreen;
+            int windowWidth = rect.Width;
+            int windowHeight = rect.Height;
+            this.Width = windowWidth;
+            this.Height = windowHeight;
+            this.BackgroundImage = System.Drawing.Image.FromFile(path);
+        }
+
+        private void SetLabel()
+        {
+            
+
+            DepLabel.Location = new Point(this.Width / 4,this.Height/6);
+            DepLabel.Font = new Font(DepLabel.Font.FontFamily, 40, FontStyle.Bold);
+
+            TitleLabel.Location = new Point(DepLabel.Location.X - 170, DepLabel.Location.Y - 100);
+            TitleLabel.Font = new Font(DepLabel.Font.FontFamily, 55, FontStyle.Bold);
+
+            CurrentStaffLabel.Location=new Point(DepLabel.Location.X,DepLabel.Location.Y+90);
+            CurrentStaffLabel.Font = new Font(DepLabel.Font.FontFamily, 40, FontStyle.Bold);
+
+            AllLabel.Location = new Point(CurrentStaffLabel.Location.X+370, CurrentStaffLabel.Location.Y);
+            AllLabel.Font= new Font(DepLabel.Font.FontFamily, 40, FontStyle.Bold);
+
+            RewardStaffLabel.Location = new Point(CurrentStaffLabel.Location.X, CurrentStaffLabel.Location.Y+90);
+            RewardStaffLabel.Font=new Font(DepLabel.Font.FontFamily, 40, FontStyle.Bold);
+
+            FirstLabel.Location = new Point(AllLabel.Location.X, AllLabel.Location.Y + 90);
+            SecondLabel.Location = new Point(FirstLabel.Location.X, FirstLabel.Location.Y + 90);
+            ThirdLabel.Location = new Point(SecondLabel.Location.X, SecondLabel.Location.Y + 90);
+
+            StartButton.Location = new Point(CurrentStaffLabel.Location.X, ThirdLabel.Location.Y + 90);
+            StartButton.Font = new Font(DepLabel.Font.FontFamily, 30, FontStyle.Bold);
+            StartButton.Size = new Size(150, 60);
+
+            StopButton.Location = new Point(StartButton.Location.X + 600, ThirdLabel.Location.Y + 90);
+            StopButton.Font = new Font(DepLabel.Font.FontFamily, 30, FontStyle.Bold);
+            StopButton.Size = new Size(150, 60);
+
+ 
+        }
+
+        private void ReadFileIn()
+        {
+            string path = Application.StartupPath+"\\Staff.txt";
+            string strLine=null;
+            string[] readArray=null;
+            string[] readArraySub = null;
+            string titleName = null;
+            try
+            {
+                FileStream aFile = new FileStream(path, FileMode.Open);
+                StreamReader sr = new StreamReader(aFile);
+
+                strLine = sr.ReadLine();
+                titleName = strLine;
+                this.TitleLabel.Text = titleName;
+                
+                strLine = sr.ReadLine();
+                readArray = strLine.Split(' ');
+                
+                this.DepAName = readArray[0];
+                this.DepBName = readArray[1];
+                this.DepCName = readArray[2];
+                this.DepDName = readArray[3];
+
+                strLine = sr.ReadLine();
+
+                while (strLine != null)
+                {
+                    readArraySub = strLine.Split(',');
+                    if(readArraySub[1]==DepAName)
+                    {
+                        DepAStaff.Add(readArraySub[0]);   
+                    }
+                    if(readArraySub[1]==DepBName)
+                    {
+                        DepBStaff.Add(readArraySub[0]);   
+                    }
+                    if(readArraySub[1]==DepCName)
+                    {
+                        DepCStaff.Add(readArraySub[0]);   
+                    }
+                    if(readArraySub[1]==DepDName)
+                    {
+                        DepDStaff.Add(readArraySub[0]);   
+                    }
+                    strLine = sr.ReadLine();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("读取错误！");
+            }
+            allList.Add(DepAStaff);
+            allList.Add(DepBStaff);
+            allList.Add(DepCStaff);
+            allList.Add(DepDStaff);
+
+        }
+
+        private void SetComboBox()
+        {
+            DepComBox.Location = new Point(DepLabel.Location.X + 380, DepLabel.Location.Y);
+            DepComBox.Font = new Font(DepLabel.Font.FontFamily, 38, FontStyle.Bold);
+            DepComBox.Items.Add(DepAName);
+            DepComBox.Items.Add(DepBName);
+            DepComBox.Items.Add(DepCName);
+            DepComBox.Items.Add(DepDName);
+
+            DepComBox.SelectedIndex = 0;
+            this.CurrentList = DepAStaff;
+
+        }
+
+        private void SetList()
+        {
+            RewardLabelList.Add(FirstLabel);
+            RewardLabelList.Add(SecondLabel);
+            RewardLabelList.Add(ThirdLabel);
+
+            foreach (Label lb in RewardLabelList)
+            {
+                lb.Font = new Font(DepLabel.Font.FontFamily, 40, FontStyle.Bold);
+            }
+
+        }
+
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (this.i >= this.CurrentList.Count-1)
+            {
+                this.i = 0;
+            }
+            else
+            {
+                this.i++;
+            }
+            AllLabel.Text = this.CurrentList[this.i];
+
+        }
+
+        
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+            timer1.Start();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            StartButton.Enabled = false;
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            this.j++;
+            if (this.j != 3)
+            {
+                this.CurrentLabel = RewardLabelList[j - 1];
+                this.CurrentLabel.Text = CurrentList[i];
+                this.CurrentList.RemoveAt(i);
+                
+            }
+            else
+            {
+                this.CurrentLabel = RewardLabelList[j - 1];
+                this.CurrentLabel.Text = CurrentList[i];
+                this.CurrentList.RemoveAt(i);
+                timer1.Stop();
+                StopButton.Enabled = false;
+            }
+
+        }
+
+        private void DepComBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.i = 0;
+            this.j = 0;
+            int index = DepComBox.SelectedIndex;
+            CurrentList = allList[index];
+            StartButton.Enabled = true;
+            StopButton.Enabled = true;
+
+            AllLabel.Text = "";
+            foreach (Label tb in RewardLabelList)
+            {
+                tb.Text = "";
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DepComBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            e.Handled = true;
+
+        }
+    }
+}
